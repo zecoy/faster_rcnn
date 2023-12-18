2bb51526ba6c9ae4e3d173ca6787d12c29cc2dee
zecoy <96514541+zecoy@users.noreply.github.com>
2023-12-18 13:26:54 +0800

    Update train_mobilenetv2.py

--- train_mobilenetv2.py
+++ train_mobilenetv2.py
@@ -15,13 +15,13 @@ from train_utils import train_eval_utils as utils
 def create_model(num_classes):
     # https://download.pytorch.org/models/vgg16-397923af.pth
     # 如果使用vgg16的话就下载对应预训练权重并取消下面注释，接着把mobilenetv2模型对应的两行代码注释掉
-    # vgg_feature = vgg(model_name="vgg16", weights_path="./backbone/vgg16.pth").features
-    # backbone = torch.nn.Sequential(*list(vgg_feature._modules.values())[:-1])  # 删除features中最后一个Maxpool层
-    # backbone.out_channels = 512
+    vgg_feature = vgg(model_name="vgg16", weights_path="./backbone/vgg16.pth").features
+    backbone = torch.nn.Sequential(*list(vgg_feature._modules.values())[:-1])  # 删除features中最后一个Maxpool层
+    backbone.out_channels = 512
 
     # https://download.pytorch.org/models/mobilenet_v2-b0353104.pth
-    backbone = MobileNetV2(weights_path="./backbone/mobilenet_v2.pth").features
-    backbone.out_channels = 1280  # 设置对应backbone输出特征矩阵的channels
+    # backbone = MobileNetV2(weights_path="./backbone/mobilenet_v2.pth").features
+    # backbone.out_channels = 1280  # 设置对应backbone输出特征矩阵的channels
 
     anchor_generator = AnchorsGenerator(sizes=((32, 64, 128, 256, 512),),
                                         aspect_ratios=((0.5, 1.0, 2.0),))
