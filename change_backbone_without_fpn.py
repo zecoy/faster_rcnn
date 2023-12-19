11ad5d1bba88a4cfd0c5989fb88915f97f5dc10a
zecoy <96514541+zecoy@users.noreply.github.com>
2023-12-19 19:44:48 +0800

    Update res50_without_fpn.py

--- change_backbone_without_fpn.py
+++ change_backbone_without_fpn.py
@@ -87,7 +87,8 @@ def main(args):
 
     # 注意这里的collate_fn是自定义的，因为读取的数据包括image和targets，不能直接使用默认的方法合成batch
     batch_size = args.batch_size
-    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    # nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    nw=4
     print('Using %g dataloader workers' % nw)
     if train_sampler:
         # 如果按照图片高宽比采样图片，dataloader中需要使用batch_sampler
